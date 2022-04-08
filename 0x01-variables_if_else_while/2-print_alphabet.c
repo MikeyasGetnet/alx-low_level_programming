ad09d05b251083c274dd9a6de51a91f65a333e25
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-08 14:21:56 -0700

    commit2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - prints a random number
+ * states whether its zero negative or positive
+ * Return: Always 0.
+ *
+ */
 int main(void)
 {
 int c;
 
-for (c=0;c<26;++c)	
+for (c = 0; c < 26; ++c)	
 putchar('a'+ c);
 
 putchar('\n');
