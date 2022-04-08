3099e6f570105149819f95001b4d9e1f83b18dbb
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-08 14:15:28 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,6 +15,6 @@ printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 else if (n % 10 == 0)
 printf("Last digit of %d is %d 0 and is 0\n", n, n % 10);
 else if (n % 10 < 6 && n % 10 != 0)
-printf("Last digit of ",n," is ", n % 10," and is less than 6 and not 0\n");
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 return (0);
 }
