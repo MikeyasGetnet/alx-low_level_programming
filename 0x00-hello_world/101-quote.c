1c5f7f5184f6450f07e65b79075f60afc0e5ed58
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-08 00:22:20 -0700

    tasl last corre

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h> // for system() 
 #include <unistd.h> // for write()
 /**
 *main - Entry point
@@ -9,6 +8,6 @@
 */
 int main(void)
 {
-write("and that piece of art is useful\"- Dora Korpar, 2015-10-19,\n");
-return (0);
+write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19,\n", 59);
+return (1);
 }
