f79a2b5e0012e22cfb1f0d986d7191e7d6b66dd8
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-08 06:12:48 -0700

    task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char c;
+for (c='a';c<'z';++c)
+{
+if(c!='q' && c!='e')
+putchar(c);
+}
+
+putchar('\n');
+
+return(0);
+}
