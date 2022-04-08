a40165e9cf89900fcfc5010026c6685c10aa0176
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-08 14:02:27 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,12 @@
 #include <time.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
-/**  
- * *main - prints a random number
- * *states whether its zero negative or positive
- * *return always 0
- * 
+/**
+ * main - prints a random number
+ * states whether its zero negative or positive
+ * Return always 0
+ *
  */
-
 int main(void)
 /*this is the main function*/
 {
