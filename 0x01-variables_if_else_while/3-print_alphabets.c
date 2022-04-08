43ed81314e21f0e261908ab7f55afeb60eec857a
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-08 06:04:57 -0700

    task 3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int c;
+for (c=0;c<26;++c)
+putchar('a' + c);
+for (c=0;c<26;+cc)
+putchar('A' + c);
+
+putchar('\n');
+
+return(0);
+}
