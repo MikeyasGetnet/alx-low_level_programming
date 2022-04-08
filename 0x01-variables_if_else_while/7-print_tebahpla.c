e6c339cc477410f89a0578193ac0be5ce1180ba4
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-08 06:33:34 -0700

    task 7 commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char n;
+for (n=z;n>=a;--n)
+putchar(n);
+
+putchar('\n');
+
+return(0);
+}
