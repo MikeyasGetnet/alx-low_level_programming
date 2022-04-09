180479be755a0fa5fb9599a28c5377565802ad32
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-09 00:45:22 -0700

    task 4 correction

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - prints a random number
+ * states whether its zero negative or positive
+ * Return: Always 0.
+ *
+ */
 int main(void)
 {
 char c;
-for (c='a';c<'z';++c)
+for (c = 'a'; c < 'z'; ++c)
 {
-if(c!='q' && c!='e')
+if(c != 'q' && c != 'e')
 putchar(c);
 }
 
 putchar('\n');
 
-return(0);
+return (0);
 }
