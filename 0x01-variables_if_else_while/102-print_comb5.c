2f78a1c21b4dc5dd6beb9ddab7c016f67d19bd0c
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-09 09:01:56 -0700

    commit task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *main - prints a random number
+ *states whether its zero negative or positive
+ *Return: Always 0.
+ */
+int main(void)
+{
+int n1, n2;
+for (n1 = 0; n1 <= 98; n1++)
+{
+for (n3 = n2 + 1; n3 <= 99; n3++)
+{
+putchar((n1 / 10) + '0');
+putchar((n1 % 10) + '0');
+putchar(' ');
+putchar((n3 / 10) + '0');
+putchar((n3 % 10) + '0');
+
+if (n1 == 98 && n3 == 99)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
