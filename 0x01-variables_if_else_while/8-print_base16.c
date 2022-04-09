78631eccbbb519dc7cf0e3a5c3516c3ae2d531f1
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-09 01:18:09 -0700

    task 8 commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,24 @@
+#include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - prints a random number
+ * states whether its zero negative or positive
+ * Return: Always 0.
+ *
+ */
 int main(void)
 {
 char c;
-for (c='1';c<='9';++c)
+for (c = '1'; c <= '9'; ++c)
 putchar(c);
-for (c='a';c<='f';++c)
+for (c = 'a';c <= 'f'; ++c)
 putchar(c);
 
 putchar('\n');
 
-return(0);
+return (0);
 }
