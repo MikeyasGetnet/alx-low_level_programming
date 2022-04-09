3e12612845aa1712a7ace776b3946e2dfcb481b8
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-09 00:41:48 -0700

    task 3 correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,15 +3,21 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - prints a random number
+ * states whether its zero negative or positive
+ * Return: Always 0.
+ *
+ */
 int main(void)
 {
 char c;
-for (c=0;c<26;++c)
+for (c = 0; c < 26; ++c)
 putchar('a' + c);
-for (c=0;c<26;+cc)
+for (c = 0; c < 26; +cc)
 putchar('A' + c);
 
 putchar('\n');
 
-return(0);
+return (0);
 }
