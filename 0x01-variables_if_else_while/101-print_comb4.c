7fbdd162d50c7e3a2776204af747c1484aad5644
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-09 08:47:31 -0700

    commit task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,6 +17,7 @@ for (n1 = 0; n1 < 8; n1++)
 for (n2 = n1 + 1; n2 < 9; n2++)
 {
 for (n3 = n2 + 1; n3 < 10; n3++)
+{
 putchar((n1 % 10) + '0');
 putchar((n2 % 10) + '0');
 putchar((n3 % 10) + '0');
@@ -27,6 +28,7 @@ continue;
 
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
