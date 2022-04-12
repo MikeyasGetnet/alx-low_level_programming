eaa5c98d7f396a177d0c2fd0ead034dc4da72276
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-12 11:05:50 -0700

    task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *main - prints a random number
+ *states whether its zero negative or positive
+ *Return: Always 0.
+ */
+int main(void)
+{
+string mi="_putchar";
+putchar(mi);
+putchar('\n');
+return (0);
+}
