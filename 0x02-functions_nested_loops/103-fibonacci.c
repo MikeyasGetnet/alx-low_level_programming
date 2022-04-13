9b30adeecfbe757affb540aaaa87a2149c65f312
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-13 00:37:58 -0700

    commit task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
+ */
+int main(void)
+{
+int i;
+unsigned long fib1 = 0, fib2 = 0 sum = 0, total =0;
+while (1)
+{
+sum = fib1 + fib2;
+if (sum > 4000000)
+break;
+if ((sum % 2) == 0)
+total += sum;
+
+fib1 = fib2;
+fib2 = sum;
+}
+
+printf("%.0f\n", total);
+
+return (0);
+}
