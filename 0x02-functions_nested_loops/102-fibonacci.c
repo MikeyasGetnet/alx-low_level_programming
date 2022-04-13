24650b8cc1259a0824323f2c0dfb9fcf9fcb0717
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-13 00:13:43 -0700

    commit 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+0;10;1c0;10;1c0;10;1c#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *main - prints a random number
+ *states whether its zero negative or positive
+ *Return: Always 0.
+ */
+int main(void)
+{
+int i;
+ unsigned long fib1 = 0, fib2 = 1; sum = 0;
+for (i = 0; i < 50; i++)
+{
+sum = fib1 + fib2;
+printf('%lu', sum);
+fib1 = fib2;
+fib2 = sum;
+if (i == 49)
+printf('\n');
+printf(', ');
+
+}
+return (0);
+}
