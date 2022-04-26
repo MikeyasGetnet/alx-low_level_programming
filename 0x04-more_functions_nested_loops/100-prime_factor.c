776df68ca6196fef75cc60d72e1a339edb03f651
Mikeyas Getnet <mikeyasg@gmail.com>
2022-04-26 12:07:38 -0700

    advanced task largest prime factor commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 /**
- * main - prints prime factors of 612852475143
+ * main - prints the largest prime factor of 612852475143
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+#include <stdio.h>
+
+void main()
 {
-return (0);
+long long number;
+number = 612852475143;
+oddPrimeNum = 3;
+maxPrimeNum = 0;
+
+while (number % 2 == 0)
+{
+maxPrimeNum = 2;
+number /= 2;
+}
+while (number != 1)
+{
+while (number % oddPrimeNum == 0)
+{
+maxPrimeNum = oddPrimeNum;
+number /= oddPrimeNum;
+}
+oddPrimeNum += 2;
+}
 }
