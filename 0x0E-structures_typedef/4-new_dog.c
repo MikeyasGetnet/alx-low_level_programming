183410c0c7d9038e6daaa1137d3470e5032ea9f4
Mikeyas Getnet <mikeyasg@gmail.com>
2022-05-10 11:34:21 -0700

    commit incorrect task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,8 +18,10 @@ char *copy_name, *copy_owner;
 unsigned int x, name_len = 0, owner_len = 0;
 
 new_name = malloc(sizeof(dog_t));
+
 if (name == NULL)
 return (NULL);
+
 if (name == NULL || age <= 0 || owner == NULL)
 {
 free(new_name);
@@ -33,17 +35,18 @@ for (x = 0; owner[x] != '\0'; x++)
 owner_len++;
 
 copy_name = malloc(sizeof(char) * (name_len + 1));
+
 if (copy_name == NULL)
 return (NULL);
 
 copy_owner = malloc(sizeof(char) * (owner_len + 1));
+
 if (copy_owner == NULL)
 return (NULL);
 
 for (x = 0; x <= name_len; x++)
 															copy_name[x] = name[x];
 															for (x = 0; x <= owner_len; x++)
-
 copy_owner[x] = owner[x];
 															new_name->name = copy_name;
 new_name->owner = copy_owner;
